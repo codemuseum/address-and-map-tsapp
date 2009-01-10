309e450082e8e13960d30152c1d6a35eddd2a68e
Matthew Moore <matt@thrivesmart.com>
2009-01-09 21:58:54 -1000

    deploy file for slicehost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,14 +19,17 @@ config.action_controller.perform_caching             = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
-config.action_controller.asset_host                  = "http://address-and-map.heroku.com"
-
+# config.action_controller.asset_host                  = "http://address-and-map.heroku.com"
+config.action_controller.asset_host                  = "209.20.86.103"
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
+# CONFIG_GMAPS_KEY = 'ABQIAAAAM1Xg3KS4pBFHn63v6AkVtBR8bIw_VtH5wPYybJ17-EVbyyZ8WhQrTCi_biWB3ktBLeNgq1l2feE-mw'
+# CONFIG_GMAPS_DOMAIN = 'address-and-map.heroku.com'
 
-CONFIG_GMAPS_KEY = 'ABQIAAAAM1Xg3KS4pBFHn63v6AkVtBR8bIw_VtH5wPYybJ17-EVbyyZ8WhQrTCi_biWB3ktBLeNgq1l2feE-mw'
-CONFIG_GMAPS_DOMAIN = 'address-and-map.heroku.com'
+CONFIG_GMAPS_KEY = 'ABQIAAAAM1Xg3KS4pBFHn63v6AkVtBQL1Zo14raxyPEZGZ2LmjYD5hqRDhQZd-D3Uyg9g_WSILpqk81x7Jvzww'
+CONFIG_GMAPS_DOMAIN = '209.20.86.103'
 
-
+# FIXME when http://code.google.com/p/phusion-passenger/issues/detail?id=169 is fixed
+config.action_controller.relative_url_root = "/address-and-map"
